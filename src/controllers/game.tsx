9703202df88be13a82a866a92e314fc469b400ae
Andrew Lucas <sqweelygig@googlemail.com>
2019-10-03 18:38:56 +0100

    A pink cube, rotating around the camera

--- src/controllers/game.tsx
+++ src/controllers/game.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as Three from "three";
 
-interface ThreeDimensionalCoordinate {
+// TODO typedef
+// tslint:disable-next-line:no-var-requires
+const OrbitControls: any = require("three-orbit-controls")(Three);
+
+interface Location {
 	east: number;
 	north: number;
 	up: number;
@@ -17,21 +22,88 @@ interface BlockType {
 }
 
 interface Block {
-	location: ThreeDimensionalCoordinate;
+	location: Location;
 	type: BlockType;
 }
 
-interface GameState {
+interface GameProps {
 	blocks: Block[];
 }
 
-class Game extends React.Component<{}, GameState> {
+class Game extends React.Component<GameProps> {
+	private mount: HTMLDivElement;
+	private scene: Three.Scene;
+	private camera: Three.PerspectiveCamera;
+	private renderer: Three.WebGLRenderer;
+	// TODO: typedef
+	private controls: any;
+	private cube: Three.Mesh;
+	private frameId: number;
+
+	public constructor(props: GameProps) {
+		super(props);
+	}
+
+	public componentDidMount(): void {
+		const width = this.mount.clientWidth;
+		const height = this.mount.clientHeight;
+
+		this.scene = new Three.Scene();
+		this.camera = new Three.PerspectiveCamera(75, width / height);
+		this.renderer = new Three.WebGLRenderer();
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+		this.renderer.setSize(width, height);
+		this.mount.appendChild(this.renderer.domElement);
+
+		this.controls.rotateSpeed = 1.0;
+		this.controls.zoomSpeed = 1.2;
+		this.controls.panSpeed = 0.8;
+
+		this.camera.position.x = 0;
+		this.camera.position.y = 0;
+		this.camera.position.z = 4;
+
+		const geometry = new Three.BoxGeometry(1, 1, 1);
+		const material = new Three.MeshBasicMaterial({
+			color: 0xff00ff,
+		});
+		this.cube = new Three.Mesh(geometry, material);
+		this.scene.add(this.cube);
+
+		this.animate();
+	}
+
+	public componentWillUnmount(): void {
+		cancelAnimationFrame(this.frameId);
+		this.mount.removeChild(this.renderer.domElement);
+	}
+
 	public render(): React.ReactElement {
-		return <canvas />;
+		return (
+			<div>
+				<div
+					id="boardCanvas"
+					style={{ width: "80vw", height: "40vw" }}
+					ref={(mount) => {
+						if (mount) {
+							this.mount = mount;
+						}
+					}}
+				/>
+			</div>
+		);
+	}
+
+	private animate(): void {
+		this.camera.rotateY(0.1);
+		this.renderer.render(this.scene, this.camera);
+		this.frameId = window.requestAnimationFrame(() => {
+			this.animate();
+		});
 	}
 }
 
-const game: Game = new Game({});
 const simpleNave: BlockType = {
 	colour: {
 		hue: 0,
@@ -66,5 +138,5 @@ const simpleChapel: Block[] = [
 		type: simpleNave,
 	},
 ];
-game.setState({ blocks: simpleChapel });
-ReactDOM.render(game.render(), document.body);
+const game = <Game blocks={simpleChapel} />;
+ReactDOM.render(game, document.body);
