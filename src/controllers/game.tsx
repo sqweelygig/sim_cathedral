93047eb37b6dadb011998a2092c854fa597e15d5
Andrew Lucas <sqweelygig@googlemail.com>
2019-10-04 07:49:12 +0100

    Added some basic camera logic

--- src/controllers/game.tsx
+++ src/controllers/game.tsx
@@ -28,6 +28,12 @@ class Game extends React.Component<GameProps> {
 	private camera: Three.PerspectiveCamera;
 	private renderer: Three.WebGLRenderer;
 	private frameId: number;
+	private maxX: number = 0;
+	private maxY: number = 0;
+	private maxZ: number = 0;
+	private minX: number = 0;
+	private minY: number = 0;
+	private minZ: number = 0;
 
 	public constructor(props: GameProps) {
 		super(props);
@@ -68,6 +74,12 @@ class Game extends React.Component<GameProps> {
 					color: block.type.colour,
 				}),
 			);
+			this.maxX = Math.max(this.maxX, block.location.east + 0.5);
+			this.minX = Math.min(this.minX, block.location.east - 0.5);
+			this.maxZ = Math.max(this.maxZ, block.location.north + 0.5);
+			this.minZ = Math.min(this.minZ, block.location.north - 0.5);
+			this.maxY = Math.max(this.maxY, block.location.up + 1);
+			this.minY = Math.min(this.minY, block.location.up);
 			cube.position.y = block.location.up + 0.5;
 			cube.position.x = block.location.east;
 			cube.position.z = block.location.north;
@@ -94,12 +106,19 @@ class Game extends React.Component<GameProps> {
 
 	private animate(): void {
 		const rightNow = new Date().getTime();
-		const radius = 2;
+		const radius = Math.max(
+			this.maxX - this.minX,
+			this.maxZ - this.minZ,
+			this.maxY - this.minY,
+		);
 		const frequency = 1 / 2000;
-		this.camera.position.x = radius * Math.sin(rightNow * frequency);
-		this.camera.position.y = 1.2;
-		this.camera.position.z = radius * Math.cos(rightNow * frequency);
-		this.camera.lookAt(0, 0.5, 0);
+		const centreX = (this.maxX + this.minX) / 2;
+		const centreY = (this.maxY + this.minY) / 2;
+		const centreZ = (this.maxZ + this.minZ) / 2;
+		this.camera.position.x = radius * Math.sin(rightNow * frequency) + centreX;
+		this.camera.position.y = this.maxY + 0.25;
+		this.camera.position.z = radius * Math.cos(rightNow * frequency) + centreZ;
+		this.camera.lookAt(centreX, centreY, centreZ);
 		this.renderer.render(this.scene, this.camera);
 		this.frameId = window.requestAnimationFrame(() => {
 			this.animate();
