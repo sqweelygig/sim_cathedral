cdc155cf92c9fee36e41e406a91ffae739958c9e
Andrew Lucas <sqweelygig@googlemail.com>
2019-10-21 15:24:49 +0100

    Moved client files into dedicated folder, in preparation for a server folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const Copy = require("copy-webpack-plugin");
 
 module.exports = {
 	entry: {
-		game: Path.join(__dirname, "src", "controllers", "game.tsx"),
+		game: Path.join(__dirname, "client", "source", "controllers", "game.tsx"),
 	},
 	mode: "development",
 	module: {
@@ -21,14 +21,14 @@ module.exports = {
 		tls: "empty",
 	},
 	output: {
-		path: Path.join(__dirname, "dist"),
+		path: Path.join(__dirname, "client", "build"),
 		filename: "[name].js",
 	},
 	plugins: [
 		new Copy([
-			{ from: Path.join("lib", "icons", "*.png"), to: Path.join("icons", "[name].png") },
-			{ from: Path.join("lib", "*.html"), to: "[name].html" },
-			{ from: Path.join("lib", "*.css"), to: "[name].css" },
+			{ from: Path.join("client", "library", "icons", "*.png"), to: Path.join("icons", "[name].png") },
+			{ from: Path.join("client", "library", "*.html"), to: "[name].html" },
+			{ from: Path.join("client", "library", "*.css"), to: "[name].css" },
 		]),
 	],
 	resolve: {
