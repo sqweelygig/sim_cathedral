5acf6e0a9e926f5610d7d23259506599ce1b0640
Andrew Lucas <sqweelygig@googlemail.com>
2019-10-21 16:13:12 +0100

    Added really simple static content server

--- server/source/main.ts
+++ server/source/main.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+/* tslint:disable:no-console */
+import * as Express from "express";
+import * as Path from "path";
+
+const app = Express();
+const port = process.env.PORT || 80;
+
+app.get("/", (request, response) => {
+	const root = Path.join(__dirname, "..", "..");
+	const index = Path.join(root, "client", "build", "game.html");
+	response.sendFile(index);
+});
+app.use("/", Express.static(Path.join("client", "build")));
+app.use("/client", Express.static(Path.join("client", "build")));
+
+app.listen(port, () => {
+	console.log("============");
+	console.log("Listening", port);
+});
